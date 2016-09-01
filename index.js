f5ee1972b50159e5512efb338f01bee74a0ca0db
Subhamoy <subhamoydboss@gmail.com>
2016-09-01 19:20:58 +0000

    Committing from c9.io

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.get('/webhook', function (req, res) {
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
+    
+    
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
